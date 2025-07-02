52a4606fd5d2854c8dc3b56c91795088cb4cafb9
Kawtar Bouarfa <kbouarfa@c2r2p14.1337.ma>
2025-07-02 13:08:03 +0100

    simple get_next_line for exam

---
+++ get_next_line.c
@@ -0,0 +1,38 @@
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+char *gnl(int fd)
+{
+    static char buf[BUFFER_SIZE];
+    static int n_read;
+    static int pos;
+    int i = 0;
+    char *res = malloc(1000);
+
+    if (fd < 0 || BUFFER_SIZE <= 0 || !res)
+        return NULL;
+
+    while (1)
+    {
+        if (pos >= n_read)
+        {
+            n_read = read(fd, buf, BUFFER_SIZE);
+            pos = 0;
+            if (n_read <= 0)
+                break;
+        }
+        if ((res[i++] = buf[pos++]) == '\n')
+            break;
+    }
+    res[i] = '\0';
+
+    if (i == 0)
+    {
+        free(res);
+        return NULL;
+    }
+    return res;
+}
